e5e9a40a1fc515c4f1bb60ef7f6ec82c7c086eb7
Andre Heß <andre.hess@myhammer.de>
2016-08-19 16:16:48 +0200

    added: navigation helper changed: playing radio station if app is launched by notification opening (untested)

---
+++ ClockRadio/ClockRadio/HelperManager/NavigationHelper.h
@@ -0,0 +1,13 @@
+//
+//  NavigationHelper.h
+//  ClockRadio
+//
+//  Created by Andre Hess on 19.08.16.
+//  Copyright © 2016 Andre Hess. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NavigationHelper : NSObject
+
+@end
---
+++ ClockRadio/ClockRadio/HelperManager/NavigationHelper.m
@@ -0,0 +1,13 @@
+//
+//  NavigationHelper.m
+//  ClockRadio
+//
+//  Created by Andre Hess on 19.08.16.
+//  Copyright © 2016 Andre Hess. All rights reserved.
+//
+
+#import "NavigationHelper.h"
+
+@implementation NavigationHelper
+
+@end
